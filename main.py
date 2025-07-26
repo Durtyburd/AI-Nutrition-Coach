60b42f646087cd4f1105e20835e1e3c4e8a431e9
Seth Perritt <sethperritt@yahoo.com>
2025-07-25 23:45:26 -0400

    Added feature to enable/disable the text-to-speech

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ def create_character():
     height = input(f'What is your height(in)? \n' + "> ")
     bmi = (int(weight) / (int(height) ** 2)) * 703
     client_goal = input(f'What is your goal for this week?')
+    print("Creating meal-plan...")
     prompt = f'This is the persons name: {name}, this is their age: {age}, this is their weight in bmi: {bmi}, this is their goal: {client_goal}. Could you give me a weekly meal plan? First start by reiterating my name, age, bmi, and client goal in a list format. Then, give me three meals a day, 7 days a week. Include the recipe. Also list the protein, carbs, and fats for each meal.'
 
     response = client.responses.create(
     model="gpt-4.1",
-    input=f'This is the persons name: {name}, this is their age: {age}, this is their weight in bmi: {bmi}, this is their goal: {client_goal}. Could you give me a weekly meal plan? First start by reiterating my name, age, bmi, and client goal in a list format. Then, give me three meals a day, 7 days a week. Include protein, carbs, and fats for each meal.' 
+    input=prompt
 )   
     x = datetime.datetime.now()
     current_date = x.strftime('%Y-%m-%d')
@@ -49,18 +50,24 @@ game_over = False
 load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 while not game_over:
-    print(f"Please enter a corresponding input. \n" + "1. Male Voice \n" + "2. Female Voice \n" + "3. Quit")
-    user_input = input("> ")
-    if user_input == "1":
-        character = create_character()
-        text_to_speech(0, character)
+    enable_tts = input(f"Do you want to enable text-to-speech? \n" + "1. Yes \n" + "2. No \n" + "> ")
+    if enable_tts.lower() == "yes" or enable_tts == "1":
+        print(f"Please enter a corresponding input. \n" + "1. Male Voice \n" + "2. Female Voice \n" + "3. Quit")
+        user_input = input("> ")
+        if user_input == "1":
+            character = create_character()
+            text_to_speech(0, character)
 
-    if user_input == "2":
-        character = create_character()
-        text_to_speech(1, character)
+        if user_input == "2":
+            character = create_character()
+            text_to_speech(1, character)
 
-    if user_input == "3":
-        game_over = True
+        if user_input == "3":
+            game_over = True
+    if enable_tts.lower() == "no" or enable_tts == "2":
+        create_character()
+        print("Your weekly meal-plan has been created.")
+        exit()
 
 
 
