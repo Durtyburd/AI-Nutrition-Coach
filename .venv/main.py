55162ec409ef8e2b58274dc88a345c5e543d7a4c
Seth Perritt <sethperritt@yahoo.com>
2025-07-25 20:16:50 -0400

    Changed functionality: App now gets a 7 day meal plan based on users bmi and goals. The meal plan is AI generated and will print to a txt file

--- .venv/main.py
+++ .venv/main.py
@@ -1,40 +1,43 @@
 import pyttsx3
 from openai import OpenAI
+import datetime
 
-def create_response(character):
+def create_character():
+    name = input("Hello, what is your name? \n"  + "> ")
+    age = input(f'Nice to meet you {name}! \n' + "What is your age? \n" + "> ")
+    weight = input(f'What is your weight(lbs)? \n' + "> ")
+    height = input(f'What is your height(in)? \n' + "> ")
+    bmi = (int(weight) / (int(height) ** 2)) * 703
+    client_goal = input(f'What is your goal for this week?')
+    prompt = f'This is the persons name: {name}, this is their age: {age}, this is their weight in bmi: {bmi}, this is their goal: {client_goal}. Could you give me a weekly meal plan? First start by reiterating my name, age, bmi, and client goal in a list format. Then, give me three meals a day, 7 days a week. Include the recipe. Also list the protein, carbs, and fats for each meal.'
+
     response = client.responses.create(
     model="gpt-4.1",
-    input="Tell me about " + character + "."
-)
+    input=f'This is the persons name: {name}, this is their age: {age}, this is their weight in bmi: {bmi}, this is their goal: {client_goal}. Could you give me a weekly meal plan? First start by reiterating my name, age, bmi, and client goal in a list format. Then, give me three meals a day, 7 days a week. Include protein, carbs, and fats for each meal.' 
+)   
+    x = datetime.datetime.now()
+    current_date = x.strftime('%Y-%m-%d')
+    file_name = f"{name}_{current_date}_nutrition_plan.txt"
+    with open(file_name, 'w') as file:
+        file.write(response.output_text)
+
+    
     return response.output_text
 
-def text_to_speech(num, gender, character):
+def text_to_speech(gender, character):
+    #Initializing
     engine = pyttsx3.init()
 
+    #Properties
     engine.setProperty('rate', 200) 
     engine.setProperty('volume',5.0)        
     voices = engine.getProperty('voices')      
-    engine.setProperty('voice', voices[num].id)  # changing index, changes voices. 0 for male, 1 for female
-    if gender == "male":
-        if character == "1":
-            text = create_response("Santa Clause")
-        if character == "2":
-            text = create_response("Abraham Lincoln")
-        if character == "3":
-            text = create_response("Robin Williams")
-        else:
-            text = create_response(character)
-        engine.say(text)
-    if gender == "female":
-        if character == "1":
-            text = create_response("Tooth Fairy")
-        if character == "2":
-            text = create_response("Harriet Tubbman")
-        if character == "3":
-            text = create_response("Amy Winehouse")
-        else:
-            text = create_response(character)
-        engine.say(text)
+    engine.setProperty('voice', voices[gender].id)  # changing index, changes voices. 0 for male, 1 for female
+
+    if gender == 0:
+        engine.say(character)
+    if gender == 1:
+        engine.say(character)
 
     engine.runAndWait()
     engine.stop()
@@ -43,21 +46,15 @@ def text_to_speech(num, gender, character):
 game_over = False
 client = OpenAI()
 while not game_over:
-    print(f"Please enter a corresponding input. \n" + "1. Male Text-To-Speech \n" + "2. Female Text-To-Speech \n" + "3. Quit")
+    print(f"Please enter a corresponding input. \n" + "1. Male Voice \n" + "2. Female Voice \n" + "3. Quit")
     user_input = input("> ")
     if user_input == "1":
-        print("Who would you like to talk to?")
-        character = input("1. Santa Clause \n" + "2. Abraham Lincoln \n" + "3. Robin Williams \n" + "4. You Choose \n" + "> ")
-        if character == "4":
-            character = input("Who do you choose (please pick a male): ")
-        text_to_speech(0, "male", character)
+        character = create_character()
+        text_to_speech(0, character)
 
     if user_input == "2":
-        print("Who would you like to talk to?")
-        character = input("1. Tooth Fairy \n" + "2. Harriet Tubbman \n" + "3. Amy Winehouse \n" + "4. You Choose \n" + "> ")
-        if character == "4":
-            character = input("Who do you choose (please pick a female): ")
-        text_to_speech(1, "female", character)
+        character = create_character()
+        text_to_speech(1, character)
 
     if user_input == "3":
         game_over = True
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # text_to_speech
 
 cd into .venv
+
+pip install
+
+python main.py
